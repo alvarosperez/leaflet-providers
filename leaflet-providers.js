914f20e01445172dd35bf9f99a94072102bc6176
alvaro sanchez <alvaro.sanchezperez@telefonica.com>
2018-05-09 18:20:34 +0200

    fix https carto tiles

--- leaflet-providers.js
+++ leaflet-providers.js
@@ -499,7 +499,7 @@
 			}
 		},
 		CartoDB: {
-			url: 'http://{s}.basemaps.cartocdn.com/{variant}/{z}/{x}/{y}.png',
+			url: '//{s}.basemaps.cartocdn.com/{variant}/{z}/{x}/{y}.png',
 			options: {
 				attribution: '{attribution.OpenStreetMap} &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
 				subdomains: 'abcd',
